758911c5e52a2dd8ca1631631c0da613b1defcf1
User <sanjarbek.89.ss@gmail.com>
2023-05-23 16:41:42 +0500

    plot data dynamically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import plotly.express as px
 st.title("Wheather forecast for the next days")
 
 place = st.text_input("Place:")
@@ -7,3 +8,14 @@ days= st.slider("Forecast Days", min_value=1, max_value=5, help= "Select the num
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
 st.subheader(f"{option} for the next {days} days in {place}")
+
+def get_data(days):
+    dates = ["2022-25-10", "2022-26-10", "2022-27-10"]
+    temperatures = [11,12,15]
+    temperatures=[days*i for i in temperatures]
+    return dates, temperatures
+
+d, t = get_data(days)
+
+figure = px.line(x= d, y=t, labels={"x":"Date", "y":"Temperature"})
+st.plotly_chart(figure)
